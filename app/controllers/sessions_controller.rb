16ce990ca00bc4159d7a41b96e684646c9f778db
Afaq Khan <afaqk3203@gmail.com>
2023-10-31 18:55:59 +0500

    create routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+
+  def new
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "chatroom#index"
+
+  get 'login', to: 'sessions#new'
+
 end
