f1f1b575cd13ea7f66262387781169cbff1d8b43
Afaq Khan <afaqk3203@gmail.com>
2023-11-02 01:39:27 +0500

    loggin bug bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,30 @@ class SessionsController < ApplicationController
   def new
 
   end
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user
+      if user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:success] = "Congrats, you have logged in"
+        redirect_to root_path
+      else
+        flash[:error] = "Invalid password"
+        redirect_to login_path
+      end
+    else
+      flash[:error] = "User not found"
+      redirect_to login_path
+    end
+  end
 
 
+  def destroy
+    session[:user_id]=nil
+
+    flash[:success]="You have logged out"
+    redirect_to login_path
+
+  end
 
 end
