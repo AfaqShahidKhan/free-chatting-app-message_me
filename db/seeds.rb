6ad3d64308dd4f6cc08eb6f20427d3484199d445
Afaq Khan <afaqk3203@gmail.com>
2023-11-01 22:32:21 +0500

    create user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(username:"Ahsan", password_digest:"1234")
+User.create(username:"Bilal", password_digest:"1234")
+User.create(username:"Mushaid", password_digest:"1234")
