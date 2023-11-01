e95dc10d1cd901e9a628462877bc14f6c3be2741
Afaq Khan <afaqk3203@gmail.com>
2023-11-01 22:41:38 +0500

    create message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_171529) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_173336) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
