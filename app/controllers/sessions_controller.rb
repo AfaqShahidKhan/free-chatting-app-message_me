eaa4d93cb87c4a412f9e74ef64fde020fa64e8f8
Afaq Khan <afaqk3203@gmail.com>
2023-11-02 21:37:35 +0500

    error handle by message partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,20 +10,20 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-     session[:user_id] = user.id
-     flash[:notice]= "You have logged in successfully"
-     redirect_to root_path
-     else
-       flash.now[:alert]="There is some error in your login"
-       redirect_to login_path
-
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash[:error] = "There was something wrong with your login information"
+      render 'new'
     end
   end
+
 
   def destroy
     session[:user_id]=nil
 
-    flash[:success]="You have logged out"
+    flash[:alert]="You have logged out"
     redirect_to login_path
 
   end
