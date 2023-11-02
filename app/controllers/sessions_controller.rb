b24c332f01c97f38b2091e36e38e30f4a69c1333
Afaq Khan <afaqk3203@gmail.com>
2023-11-02 15:53:02 +0500

    login bug resolved

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,21 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+
+
   def create
     user = User.find_by(username: params[:session][:username])
-    if user
-      if user.authenticate(params[:session][:password])
-        session[:user_id] = user.id
-        flash[:success] = "Congrats, you have logged in"
-        redirect_to root_path
-      else
-        flash[:error] = "Invalid password"
-        redirect_to login_path
-      end
-    else
-      flash[:error] = "User not found"
-      redirect_to login_path
+    if user && user.authenticate(params[:session][:password])
+     session[:user_id] = user.id
+     flash[:notice]= "You have logged in successfully"
+     redirect_to root_path
+     else
+       flash.now[:alert]="There is some error in your login"
+       redirect_to login_path
+
     end
   end
-
 
   def destroy
     session[:user_id]=nil
