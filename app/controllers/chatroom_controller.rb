f95df2dbabb4a663207b297740f61cd02078ede6
Afaq Khan <afaqk3203@gmail.com>
2023-11-02 23:03:02 +0500

    add messages through ui

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,13 @@
 class ChatroomController < ApplicationController
 
+  before_action :require_user
+
+def new
+  @message= Message.new
+end
+
 def index
+  @message= Message.new
 @messages= Message.all
 end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-
+before_action :logged_in_redirect_error, only: [:new, :create]
   def new
 
   end
@@ -26,6 +26,16 @@ class SessionsController < ApplicationController
     flash[:alert]="You have logged out"
     redirect_to login_path
 
+  end
+
+
+  private
+
+  def logged_in_redirect_error
+    if logged_in?
+      flash[:error]="You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
